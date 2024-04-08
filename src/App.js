1882b883e24eeaea582e851b11135bfb7f8131a4
manbonad <114388931+manbonad@users.noreply.github.com>
2024-04-08 10:32:30 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-calculadora'>
+        <Pantalla />
+      <div className='fila'>
+        <Boton>1</Boton>
+        <Boton>+</Boton>
+        </div>
+      </div>
     </div>
   );
 }
